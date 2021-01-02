4c8fb828a766250ac2ad0625dfa89cd5bd3b70f1
leehyunjong <ahajongs@gmail.com>
2021-01-03 03:06:31 +0900

    [Add] login, token 추가

--- service/index.js
+++ service/index.js
@@ -1,3 +1,4 @@
 module.exports={
-    communicationService:require('./communicationService')
+    communicationService:require('./communicationService'),
+    userService:require('./userService')
 }
