acfe366eaf38c34a5a0e0bccb16f1b5ec1bff86e
leehyunjong <ahajongs@gmail.com>
2021-01-07 16:19:13 +0900

    [Add] getHouseInfo, getNotice 추가

--- service/houseInfoService.js
+++ service/houseInfoService.js
@@ -1,8 +1,20 @@
-const {User,HouseInfo} = require('../models')
+const {User,HouseInfo,Notice} = require('../models')
+const sequelize = require('sequelize')
+const {Op} = sequelize
 
 module.exports={
     getUnit:async(address)=>{
         const unitList = await User.findAll({where:{type:1,address:address},attributes:['id','unit','house_info_id']})
         return unitList
+    },
+    getHouseInformation:async(id)=>{
+        const house_info_id = (await User.findByPk(id)).house_info_id
+        const houseInfo = await HouseInfo.findOne({where:{id:house_info_id},attributes:{exclude:['createdAt','updatedAt']}})
+        const notice = await Notice.findAll({where:{house_info_id},order:[['post_writer','DESC']],attributes:['id','notice_title','notice_contents']})
+        return {houseInfo,notice}
+    },
+    getNoticeDetail:async(id)=>{
+        const notice = await Notice.findOne({where:{id},attributes:{exclude:['createdAt','updatedAt','HouseInfoId']}})
+        return notice
     }
 }
