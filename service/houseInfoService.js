13ef4f1646b085184f88b6d08fc7724b15882473
leehyunjong <ahajongs@gmail.com>
2021-01-08 21:02:54 +0900

    [Update] 공지사항 작성 수정

--- service/houseInfoService.js
+++ service/houseInfoService.js
@@ -1,6 +1,7 @@
 const {User,HouseInfo,Notice} = require('../models')
 const sequelize = require('sequelize')
 const {Op} = sequelize
+const moment = require('moment')
 
 module.exports={
     getUnit:async(address)=>{
@@ -18,7 +19,15 @@ module.exports={
         return notice
     },
     setNotice:async(house_info_id,notice_title,notice_contents,notice_option)=>{
-        const houseInfo = await findByPk(house_info_id)
-        const newNotice = await Notice.create({})
+        const houseInfo = await HouseInfo.findByPk(house_info_id)
+        await notice_option.map(async v=>{
+            console.log('v:',v)
+            const convert_date = moment(v.date)
+            const year = convert_date.year()
+            const month = convert_date.month()+1
+            const date = convert_date.date()
+            const newNotice = await Notice.create({notice_title,notice_contents,notice_year:year,notice_month:month,notice_day:date,notice_time:v.time})
+            await houseInfo.addNotice(newNotice) 
+        })
     }
 }
